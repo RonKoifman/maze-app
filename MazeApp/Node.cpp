da34a7b7aa6e3e4fa812102f7be51d75554b3a02
Ron Koifman <ronkoifman94@gmail.com>
2019-12-09 00:15:36 +0200

    Create maze class and update the queue class

---
+++ MazeApp/Node.cpp
@@ -0,0 +1,44 @@
+#include "Node.h"
+
+Node::Node() // C'tor
+	: next(nullptr)
+{
+}
+
+Node::Node(Vertex v, Node* next) // Copy C'tor
+	: vertex(vertex), next(next)
+{
+}
+
+void Node::insertAfter(Node* newNode)
+{
+	newNode->next = next;
+	next = newNode;
+}
+
+Node* Node::deleteAfter()
+{
+	Node* temp = next;
+	if (next == nullptr) // End of the list
+	{
+		return nullptr;
+	}
+
+	next = temp->next;
+	return temp;
+}
+
+void Node::setVertex(Vertex vertex)
+{
+	this->vertex = vertex;
+}
+
+Vertex Node::getVertex()
+{
+	return vertex;
+}
+
+Node* Node::getNext()
+{
+	return next;
+}
---
+++ MazeApp/Node.h
@@ -0,0 +1,25 @@
+#ifndef __NODE_H
+#define __NODE_H
+
+#include "OutResources.h"
+#include "Vertex.h"
+
+class Node 
+{
+public:
+	Vertex vertex;
+	Node* next;
+
+	Node(); // C'tor
+	Node(Vertex vertex, Node* next = nullptr); // Copy C'tor
+	Node(Node& other) = delete; // Disable default copy c'tor
+	void insertAfter(Node* node);
+	Node* deleteAfter();
+	// Setters
+	void setVertex(Vertex vertex);
+	// Getters
+	Vertex getVertex();
+	Node* getNext();
+};
+
+#endif // __NODE_H
